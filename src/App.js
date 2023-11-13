55b5bd1a70b32d7efc9698f6a0267e4eaf3c71f1
Youngjun Choi <youngjuni21@gmail.com>
2023-11-13 21:28:52 +0900

    11.13_commit

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from "react-bootstrap/Button";
+import data from "./data.js";
+import { useState } from "react";
+import Card from "./card.js";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import Detail from "./routes/detail.js";
+
+function App() {
+  let [shoes] = useState(data);
+  let navigate = useNavigate();
+
+  return (
+    <div className="App">
+      <Navbar
+        bg="dark"
+        data-bs-theme="dark"
+        expand="lg"
+        className="bg-body-tertiary"
+      >
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link
+                onClick={() => {
+                  navigate("/");
+                }}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                onClick={() => {
+                  navigate("/detail");
+                }}
+              >
+                Detail
+              </Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Link to="/">홈</Link>
+      <Link to="/detail">상세페이지</Link>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              메인페이지임
+              <div className="main-bg"></div>
+              <div className="container">
+                <div className="row">
+                  {shoes.map((a, i) => {
+                    return <Card shoes={shoes[i]} i={i}></Card>;
+                  })}
+                </div>
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/detail/:id"
+          element={
+            <div>
+              <Detail shoes={shoes} />
+            </div>
+          }
+        />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes/detail.js
@@ -0,0 +1,29 @@
+import { useParams } from "react-router-dom";
+
+
+
+function Detail(props) {
+  let { id } = useParams();
+  let 찾은상품 = props.shoes.find((x) => {
+    return x.id == id;
+  });
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            width="100%"
+          />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}</p>
+          <button className="btn btn-danger">주문하기</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Detail;
