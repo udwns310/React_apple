8fcd60c110507ae0ff097d86bd4bedb23a4a1a61
Youngjun Choi <youngjuni21@gmail.com>
2023-11-17 22:11:00 +0900

    애니메이션

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -82,16 +82,23 @@ function Detail(props) {
   );
 }
 
-function TabContent(props) {
-  if (props.탭 == 0) {
-    return <div>내용0</div>;
-  }
-  if (props.탭 == 1) {
-    return <div>내용1</div>;
-  }
-  if (props.탭 == 2) {
-    return <div>내용2</div>;
-  }
+function TabContent({ 탭 }) {
+  let [fade, setFade] = useState("");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setFade("end");
+    }, 100);
+    return () => {
+      setFade("");
+    };
+  }, [탭]);
+
+  return (
+    <div className={"start " + fade}>
+      {[<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][탭]}
+    </div>
+  );
 }
 
 export default Detail;
